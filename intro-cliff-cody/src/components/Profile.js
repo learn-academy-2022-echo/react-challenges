49d2b7ef8b2a08b09d898bdbd7c0a08d7f5325c0
HeavyArms0511 <learnacademy@LEARNs-MacBook-Air.local>
2022-08-11 15:54:40 -0700

    Charlean said we are doing GREAT

---
+++ intro-cliff-cody/src/components/Profile.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Profile = () => {
+  return (<>
+    <ul>
+        <li>When the dog bites </li>
+        <li>When the bee stings</li>
+        <li>When I'm feeling sad</li>
+        <li>I simply remember my favorite things</li>
+        <li>And then I don't feel so bad</li>
+   </ul>
+    </>)
+  
+}
+
+export default Profile
