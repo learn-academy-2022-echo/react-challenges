952e73b05c604eee9cc0619b0b969d9e91e447e2
HeavyArms0511 <learnacademy@LEARNs-MacBook-Air.local>
2022-08-12 13:37:49 -0700

    Changing Names

--- state-cathrine-cody/src/App.js
+++ state-cathrine-cody/src/App.js
@@ -1,12 +1,25 @@
-
+import React, {useState} from 'react'
 import './App.css';
 
 
-
 function App() {
+
+  const [colors, setColors] = useState('white')
+   const color = ['white', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink']
+
+
+  const boxClick = () =>{
+    let ranColor = Math.floor(Math.random() * color.length)
+    setColors(color[ranColor])
+
+  }
+
+
   return (<>
       <h1>The Changing Color Box</h1>      
-        <p className= 'box' >White</p>
+       <div className= 'box' onClick={boxClick}> 
+          <p>{colors}</p>
+       </div>
  </> ) 
   
 }
--- state-cathrine-cody/src/components/ColorBox.js
+++ state-cathrine-cody/src/components/ColorBox.js
@@ -3,10 +3,5 @@ import React, {useState} from 'react'
 
 const colors = ['white', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink']
 
-const [color, setColor] = useState('')
-
-const changeColor = () => {
-    return
-}
 
 // export default ColorBox
