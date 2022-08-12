c9791ecf64da49c2f63261fee40e0f3b147f202d
fpsolis219 <fpsolis21995@gmail.com>
2022-08-12 11:58:22 -0700

    completed first 2 challenges included in lecture notes

---
+++ state-hieu-francisco/src/components/ColorChangeClick.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+
+const color = useState
+  const ColorChangeClick = () => {
+      document.getElementById("white box").innerHTML="blue"
+      return (
+          <>
+            <p>{"color"}</p>
+            <button onClick={"color"}>white</button>
+
+            {/* <p>Miles: {miles}</p>
+            // <button onClick={addAMile}>Add Mile</button> 
+       */}
+      
+          </>
+        )
+}
+
+
+export default ColorChangeClick
