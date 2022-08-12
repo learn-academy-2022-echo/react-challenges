15a8be9f216b2dc5d8c94e66354ee31f50008e08
roeckerc <roeckerc@gmail.com>
2022-08-12 08:00:54 -0700

    added footer and second list

--- intro-cliff-cody/src/App.js
+++ intro-cliff-cody/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header'
 import Profile from './components/Profile'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Profile/>
+      <Footer/>
     </div>
   );
 }
--- intro-cliff-cody/src/components/Profile.js
+++ intro-cliff-cody/src/components/Profile.js
@@ -2,12 +2,29 @@ import React from 'react'
 
 const Profile = () => {
   return (<>
+  <h3>Things Cody & Cliff do not like and how the solve it. </h3>
     <ul>
         <li>When the dog bites </li>
         <li>When the bee stings</li>
         <li>When I'm feeling sad</li>
         <li>I simply remember my favorite things</li>
         <li>And then I don't feel so bad</li>
+   </ul>
+   <br></br>
+   <h3>These are a few of their favorite things.</h3>
+   <ul>
+      <li>Raindrops on roses and whiskers on kittens</li>
+      <li>Bright copper kettles and warm woolen mittens</li>
+      <li>Brown paper packages tied up with strings</li>
+      
+      <li>Cream-colored ponies and crisp apple strudels</li>
+      <li>Doorbells and sleigh bells and schnitzel with noodles</li>
+      <li>Wild geese that fly with the moon on their wings</li>
+      
+      <li>Girls in white dresses with blue satin sashes</li>
+      <li>Snowflakes that stay on my nose and eyelashes</li>
+      <li>Silver-white winters that melt into springs</li>
+      
    </ul>
     </>)
   
