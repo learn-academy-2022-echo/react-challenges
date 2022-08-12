aa81427b168151cffa904e9978de8ca888cb9d74
HeavyArms0511 <learnacademy@LEARNs-MacBook-Air.local>
2022-08-12 13:02:13 -0700

    White Box

---
+++ state-cathrine-cody/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+
+
+function App() {
+  return (<>
+      <h1>The Changing Color Box</h1>      
+        <p className= 'box' >White</p>
+ </> ) 
+  
+}
+
+export default App;
---
+++ state-cathrine-cody/src/components/ColorBox.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+
+const colors = ['white', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink']
+
+const [color, setColor] = useState('')
+
+const changeColor = () => {
+    return
+}
+
+// export default ColorBox
