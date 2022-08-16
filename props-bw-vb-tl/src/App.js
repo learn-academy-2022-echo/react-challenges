05b4f7233112cab47a6144b22b021f09bc174ce2
VKathryn <vkbastien@gmail.com>
2022-08-16 16:30:18 -0700

    third react update

--- props-bw-vb-tl/src/App.js
+++ props-bw-vb-tl/src/App.js
@@ -1,36 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import './SquareBox.css';
+import './components/SquareBox.css';
 
 const App = () => {
   return <>
   <p className= "square1"></p>
+
+
+
   </>
 }
 
-// function App() {
-//   return (
-
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-      //   // </a>
-      // </header>
-//     </div>
-//   );
-// }
-
 export default App;
-// export default SquareBox;
+
+
 
