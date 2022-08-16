3e8c12ef619ed1a2aa0866bfd07670993fb953e8
luislaurel <luislaurel133@gmail.com>
2022-08-16 15:36:50 -0600

    updated

--- props-clifford-luis/src/App.js
+++ props-clifford-luis/src/App.js
@@ -1,12 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import RollDice from './Components /RollDice';
+import React, {useState} from 'react'
 
 function App() {
+
+const [numbers, setNumbers] = useState(["1","2","3","4","5","6"])
+
+const [randomIndex,setRandomIndex] = useState(0)
+
+const handleClick = () => {
+  let randomNumber = Math.floor(Math.random() * 7) 
+  setRandomIndex(randomNumber)
+}
   return (
+
     <div className="App">
       <h1 > dice game </h1>
-      <h2 className= "box"> box test</h2>
-      <h2 className= "box"> box test</h2>
+      < RollDice numbers={value} index ={randomIndex} handleClick={handleClick}/>
+      < RollDice/>
     </div>
   );
 }
