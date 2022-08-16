cca0c97bc29fa543236e74b7b183cc3f892f945b
roeckerc <roeckerc@gmail.com>
2022-08-16 13:51:30 -0700

    made the boxes

---
+++ props-clifford-luis/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 > dice game </h1>
+      <h2 className= "box"> box test</h2>
+      <h2 className= "box"> box test</h2>
+    </div>
+  );
+}
+
+export default App;
