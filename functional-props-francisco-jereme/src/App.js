ed26eaceb159d10467af8c49fc2a930d0d7aca75
fpsolis219 <fpsolis21995@gmail.com>
2022-08-17 14:44:17 -0700

    food order

---
+++ functional-props-francisco-jereme/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import Menu from './components/Menu'
+
+const App = ()=> {
+  return(
+  <>
+  <h1> Hi </h1>
+  <Menu/>
+  <orders/>
+  </>
+  )
+}
+
+export default App;
