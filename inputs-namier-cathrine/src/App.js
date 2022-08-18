53ca66c53a68576fe784eb2c46ff542f93739155
namieralsabagh <namier@me.com>
2022-08-18 16:21:34 -0700

    Stretch challenge 1

--- inputs-namier-cathrine/src/App.js
+++ inputs-namier-cathrine/src/App.js
@@ -2,31 +2,44 @@ import React, { useState } from 'react'
 import GoodRobot from './components/GoodRobot'
 import BadRobot from './components/BadRobot'
 import KanyeBot from './components/KanyeBot'
-
+import DeveloperBot from './components/DeveloperBot'
 
 const App = () => {
 
   const [userInput, setUserInput] = useState("")
-  
+  const [badInput, setBadInput] = useState("")
+  const [developInput, setDevelopInput] = useState("")
+  const handleBackspace = (e) => {
+    if(e.keyCode === 8) {
+       setBadInput(badInput.pop() && " ")
+    }
+}
   const handleChange = (e) => {
     console.log(e.target.value)
     setUserInput(e.target.value)
+    setBadInput(["Bla", ...badInput])
+    setDevelopInput(["Done for the day"])
   }
+  
  
   return (
     <>
     <h1>Robo Active Listening</h1>
     <h3>Say something</h3>
-    <input type="text" value={userInput} onChange={handleChange} />
+    <input type="text" value={userInput} onChange={handleChange} onKeyDown = {handleBackspace} />
 
     <h3>Good Robot</h3>
     <p><GoodRobot name={userInput}/></p>
 
     <h3>Bad Robot</h3>
-    <p><BadRobot name={userInput}/></p>
+    <p><BadRobot name={badInput}/></p>
+    
 
     <h3>Kanyebot 5000</h3>
     <p><KanyeBot name={userInput}/> </p>
+
+    <h3>DeveloperBot</h3>
+    <p><DeveloperBot name={developInput}/></p>
     </>
   )
 }
--- inputs-namier-cathrine/src/components/BadRobot.js
+++ inputs-namier-cathrine/src/components/BadRobot.js
@@ -1,24 +1,8 @@
 import React, { useState } from 'react'
 
-// function BadRobot() {
-//   const [state, setState] = React.useState({
-//     firstName: ""
-//   })
-//   return (
-//     <form>
-//       <label>
-//         First name
-//         <input
-//           type="text"
-//           value={state.firstName}
-//           onChange={handleChange}
-//         />
-//       </label>
-//     </form>
-//   );
-// }
-
-// function BadRobot(props) {
-//   return <p>I hear you saying {props.name}. Is that correct? </p>
-// }
+function BadRobot(props) {
+    return <p> I hear you saying{props.name}. Is that correct?</p>
+    
+}
 export default BadRobot;
+
