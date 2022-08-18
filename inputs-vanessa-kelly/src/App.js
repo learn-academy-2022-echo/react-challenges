d1f41e8185524c684c20cba8372c4e92c2373e73
kellychan56 <kelly.chan568@gmail.com>
2022-08-18 16:26:39 -0700

    completed robot fun

--- inputs-vanessa-kelly/src/App.js
+++ inputs-vanessa-kelly/src/App.js
@@ -1,25 +1,42 @@
 import React, {useState} from 'react'
 import GoodRobot from './Components/GoodRobot'
 import BadRobot from './Components/BadRobot'
+import KanyeBot from './Components/KanyeBot'
+import BackwardsBot from './Components/BackwardsBot'
+import './App.css'
+
 
 const App = () => {
   const [userInput, setUserInput] = useState("")
-  const [blahString, setBlahString] = 
+  const [blahString, setBlahString] =
   useState("")
+  const [inputReverse, setInputReverse] = useState("")
     const handleChange = (e) => {
-      console.log(e.target.value);
-        setUserInput(e.target.value)
+      console.log(e.target.value)
+      setUserInput(e.target.value)
+
+      let inputLength = e.target.value.length
+      let repeatBlah = "BLAH".repeat(inputLength)
+      setBlahString(repeatBlah.slice(0, inputLength))
+
+      setInputReverse(e.target.value.split("").reverse().join(""))
     }
-  const blahMaker = ({userInput}) => {
-    let inputLength = {userInput}.length
-    let repeatBlah = "BLAH".repeat(inputLength)
-    setBlahString(repeatBlah)
-  }
+
   return <>
-    <h1>Robot Party</h1>
+    <h1 class = "rainbow-text">Robot Party</h1>
       <input type="text" value={userInput} onChange = {handleChange}/>
-        <GoodRobot userInput= {userInput}/>
-        <BadRobot blahString = {blahString} blahMaker={blahMaker}/>
+      <div className="robots">
+        <div className="row">
+          <div className="column">
+          <GoodRobot userInput={userInput} />
+          <BadRobot blahString={blahString} />
+          </div>
+          <div className="column">
+          <KanyeBot userInput={userInput} />
+          <BackwardsBot inputReverse = {inputReverse}/>
+          </div>
+        </div>
+      </div>
   </>
 }
 
